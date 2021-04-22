9fb82014a579b8a9ad37c3958eb886a302ba488d
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-04-22 18:14:53 +0100

    Initial Commit

---
+++ Layout/LayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutApp.swift
+//  Layout
+//
+//  Created by MAC on 22/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct LayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
